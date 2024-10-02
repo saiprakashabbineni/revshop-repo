1fe4404eee738cd02dbe450075571d2c27e87702
dinesh-source <singuluridinesh@gmail.com>
2024-10-02 02:58:05 -0700

    added feedback functionality

--- src/main/java/com/project/revshop/repository/WishlistRepository.java
+++ src/main/java/com/project/revshop/repository/WishlistRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
-
-@Repository
-public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-    Set<Wishlist> findByUserUserId(Integer userId);
-    Wishlist findByUserUserIdAndProductProductId(Integer userId, Integer productId);
-	void addToWishlist(Long userId, Long productId);
-	void removeFromWishlist(Long userId, Long productId);
-}
+//
+//@Repository
+//public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+//    Set<Wishlist> findByUserUserId(Integer userId);
+//    Wishlist findByUserUserIdAndProductProductId(Integer userId, Integer productId);
+//	void addToWishlist(Long userId, Long productId);
+//	void removeFromWishlist(Long userId, Long productId);
+//}
